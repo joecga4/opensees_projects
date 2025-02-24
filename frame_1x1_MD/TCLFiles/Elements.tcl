d21e51b77ab3a3ec802931f28bcbf5ca4cf8fbaa
joecga4 <88100652+joecga4@users.noreply.github.com>
2025-02-24 16:17:00 -0500

    modified:   frame_1x1_MD/TCLFiles/Elements.tcl

--- frame_1x1_MD/TCLFiles/Elements.tcl
+++ frame_1x1_MD/TCLFiles/Elements.tcl
@@ -9,6 +9,6 @@ element  elasticBeamColumn       2       3       4  +1.990000E+02  +2.000000E+02
 # Element "vigacol":    eleTag    NodeI    NodeJ    A    E    Iz    geoTranTag    <alpha  d>    <-mass massDens>    <-cMass> 
 element  elasticBeamColumn       3       2       4  +1.990000E+02  +2.000000E+02  +3.117630E+03     1  +0.000000E+00  +0.000000E+00  -mass +0.000000E+00 
 
-# Element "trussG60":    eleTag    NodeI    NodeJ    -site siteTag    -initStif Kij    <-iMod>    <-noRayleigh>    <-rho rho> 
-expElement  truss       4       3       2  -site    1  -initStif +1.400000E+02  -rho +0.000000E+00 
+# expElement truss eleTag iNode jNode -site siteTag -initStif Kij <-tangStif tangStifTag> <-iMod> <-noRayleigh> <-rho rho> <-cMass>
+expElement truss 4 3 2 -site 1 -initStif +1.400000E+02 -tangStif +1.400000E+02 -rho +0.000000E+00 
 
